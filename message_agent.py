f43e360c7f030af5c0b9b35a1bf356f7f5708798
scobydobydoo <rawatabhi0214@gmail.com>
2025-12-12 10:02:05 +0530

    Update message_agent.py

--- message_agent.py
+++ message_agent.py
@@ -4,13 +4,18 @@ from ..tools import make_email_template
 from ..config import config
 from ..utils import suppress_output_callback
 
-email_template_tool = FunctionTool(make_email_template)
+email_tool = FunctionTool(make_email_template)
 
 message_agent = Agent(
-    name='MessageWriterAgent',
+    name="MessageWriterAgent",
     model=config.worker_model,
-    instruction="""You are MessageWriterAgent. Using planner_output and academic_output draft a polite email to a professor including student's name and course. Save to 'email_draft'.""" ,
-    tools=[email_template_tool],
-    output_key='email_draft',
+    instruction="""
+Using planner_output and academic_output from session state,
+draft a polite email a student can send to a professor requesting help.
+Use the make_email_template tool to structure the message.
+Save final email to email_draft.
+""",
+    tools=[email_tool],
+    output_key="email_draft",
     after_agent_callback=suppress_output_callback,
 )
