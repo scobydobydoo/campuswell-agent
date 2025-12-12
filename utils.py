10a40e7154ac94453acc26ad0cbc6c5a16d87e2e
scobydobydoo <rawatabhi0214@gmail.com>
2025-12-12 09:59:50 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from google.adk.agents.callback_context import CallbackContext
-from google.genai.types import Content
+from google.genai.types import Content, TextPart
+
 def suppress_output_callback(callback_context: CallbackContext) -> Content:
-    return Content()
+    return Content(role="assistant", parts=[TextPart(text="")])
